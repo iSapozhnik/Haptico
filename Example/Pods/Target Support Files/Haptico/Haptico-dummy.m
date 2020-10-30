ed1307250a5d4e0d1d50a3b030e1062a28809d70
Florian Fittschen <ffittschen@gmail.com>
2020-10-30 22:11:01 +0100

    Update Example project to use SPM

--- Example/Pods/Target Support Files/Haptico/Haptico-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Haptico : NSObject
-@end
-@implementation PodsDummy_Haptico
-@end
--- Example/Pods/Target Support Files/Pods-Haptico_Example/Pods-Haptico_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Haptico_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Haptico_Example
-@end
