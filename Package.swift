82d824f424fd49200a2ddddba0342a3d77cd3955
Justin Kumpe <37677675+justinkumpe@users.noreply.github.com>
2022-10-29 10:09:06 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Haptico",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v9),
     ],
     products: [
         .library(name: "Haptico", targets: ["Haptico"])
